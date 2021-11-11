10eb380a6e45cf2f01ba209841d943af4302f3d7
Ivan Tretyak <das.blackya@gmail.com>
2021-11-11 14:09:25 +0300

    Separate method to alone file

---
+++ Class1.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ORMDatabaseModule
+{
+    public interface ITable
+    {
+
+    }
+
+    public class DatabaseContext : DbContext
+    {
+        public DbSet<Album> Albums { get; set; }
+        public DbSet<MetaData> MetaDatas { get; set; }
+        public DbSet<Photo> Photos { get; set; }
+        public DbSet<AlbumContext> AlbumContexts { get; set; }
+        public string DbPath { get; private set; }
+        public DatabaseContext()
+        {
+            var folder = Environment.SpecialFolder.LocalApplicationData;
+            var path = Environment.GetFolderPath(folder);
+            string v = $"{path}{System.IO.Path.DirectorySeparatorChar}photos.db";
+            DbPath = v;
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        => options.UseSqlite($"Data Source={DbPath}");
+
+    }
+
+    [Index(nameof(Name), IsUnique = true)]
+    public class Album : ITable
+    {
+        public int AlbumId { get; set; }
+        public string Name { get; set; }
+        public string DateCreation { get; set; }
+    }
+    public class MetaData : ITable
+    {
+        public int MetadataId { get; set; }
+        public string Manufacturer { get; set; }
+        public string Model { get; set; }
+        public int Orientation { get; set; }
+        public int FocusLength { get; set; }
+        public float Longitude { get; set; }
+        public float Latitude { get; set; }
+        public int Flash { get; set; }
+        public string DateCreation { get; set; }
+    }
+    public class Photo : ITable
+    {
+        public int PhotoId { get; set; }
+        public string Path { get; set; }
+        public int Exist { get; set; }
+        public MetaData MetaData { get; set; }
+        public int MetaDataId { get; set; }
+    }
+    public class AlbumContext : ITable
+    {
+        public int AlbumContextId { get; set; }
+        public Album Album { get; set; }
+        public int AlbumId { get; set; }
+        public Photo Photo { get; set; }
+        public int PhotoId { get; set; }
+    }
+}
